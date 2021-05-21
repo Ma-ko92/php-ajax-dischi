649ca6554bcc2ed52e7caa06db64bf746d5be596
Mario Koco <mario.koco92@gmail.com>
2021-05-21 16:37:38 +0200

    inizio compito, svolgimento senza vuejs, header inserito

---
+++ Senza Vuejs/index.php
@@ -0,0 +1,31 @@
+<?php
+    include '../database/database.php';
+    // var_dump($database);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Php Ajax Dischi</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<!-- BODY -->
+<body>
+    <!-- HEADER -->
+    <header>
+        <div class="logo">
+            <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="logo">
+        </div>
+    </header>
+    <!-- HEADER END -->
+
+</body>
+<!-- BODY END -->
+</html>
